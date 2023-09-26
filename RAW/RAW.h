d93c68dc02760f35156ef4a62a93de77cf6616f2
IJJA3141 <alex.estella.cj@gmail.com>
2023-09-26 18:06:17 +0200

    00.00 init

--- RAW/RAW.h
+++
@@ -1,9 +0,0 @@
-#ifndef RAW
-#define RAW
-#include <vector>
-#include <string>
-#include <fstream>
-
-void read(const char *_path, std::vector<char>* _pvCharacter, std::vector<int>* _pvInt);
-
-#endif
